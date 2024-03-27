fd4716a461c516d3ed2a7bf774f0794a4c9272dd
tapp1ng <81276823+tapp1ng@users.noreply.github.com>
2024-03-27 19:43:06 -0400

    Add files via upload

---
+++ ITEC_2600B_MAIN.m
@@ -0,0 +1,18 @@
+%START OF THE MAIN
+
+%prompt if sitter or looking for sitter
+    %prompts user to search or enter information
+
+    %based on selection two options will be prompted 
+
+
+        %if search
+        %user inputs all specifics that they want (ex. age, breed, animal, etc)
+        %algoritm to seach through the database
+
+        %if enter
+        %user inputs all specifics and then store in database
+
+%based on either user is a sitter or not use the required algorithm
+%finds pair 
+     
